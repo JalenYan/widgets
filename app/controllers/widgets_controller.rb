dfa04f0be7dd1967fafd618462f9e247808ec5f4
JalenYan <jinlianggg@outlook.com>
2025-05-18 17:22:23 +0800

    Enhance widget show functionality by adding manufacturer details, including name and address, to the widget object and updating the view to display this information.

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,7 +2,20 @@ require "ostruct"
 
 class WidgetsController < ApplicationController
   def show
-    @widget = OpenStruct.new(id: params[:id], manufacturer_id: rand(100), name: "Widget #{params[:id]}")
+    manufacturer = OpenStruct.new(
+      id: rand(100),
+      name: "Sector 7G",
+      address: OpenStruct.new(
+        id: rand(100),
+        country: "CN"
+      )
+    )
+    @widget = OpenStruct.new(
+      id: params[:id],
+      manufacturer_id: manufacturer.id,
+      name: "Widget #{params[:id]}",
+      manufacturer: manufacturer
+    )
   end
 
   def index

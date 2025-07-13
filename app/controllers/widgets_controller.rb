62273499a26c8ca5e869ebc451e268a779fbdc65
JalenYan <jinlianggg@outlook.com>
2025-07-13 23:23:30 +0800

    区分 domain 和 view 关注点，domain 关注点不应该创建 helper

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -16,6 +16,15 @@ class WidgetsController < ApplicationController
       name: "Widget #{params[:id]}",
       manufacturer: manufacturer
     )
+
+    # 因为现在还没有 widget 类，所以这样演示
+    def @widget.widget_id
+      if self.id.to_s.length < 3
+        self.id.to_s
+      else
+        self.id.to_s[0..-3] + "." + self.id.to_s[-2..-1]
+      end
+    end
   end
 
   def index

886890c0c7103ee31e16ea1506d94e2c6ba4942b
JalenYan <jinlianggg@outlook.com>
2025-07-13 23:50:31 +0800

    使用 helper 去构建 html，而不是插入字符串的方式

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,8 @@
 module ApplicationHelper
-  # html_safe 是告诉 Rails 这个字符串是安全的，可以被渲染为 HTML，不用被 escape
-  # 但是，我们无法保证 widget_id 是安全的，所以这是个反面例子
-  # “Thus, when authoring helpers,
+  # When authoring helpers,
   # never build strings using interpolation or concatenation.
-  # Try to always use Rails’ helper methods to create your markup”
+  # Try to always use Rails’ helper methods to create your markup
   def styled_widget_id(widget)
-    %(
-      <span style="font-family: monospace">
-        #{widget.widget_id}
-      </span>
-    ).html_safe
+    content_tag(:span, widget.widget_id, style: "font-family: monospace")
   end
 end

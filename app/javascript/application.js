065efc5ab9f5cff1530e7d9528d4e8f9da8c06e3
JalenYan <jinlianggg@outlook.com>
2025-07-16 23:06:39 +0800

    减少 Turbo 进度条的延时时间到 100ms，这有助于用户体验

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+
+// The default of 500ms is too long and
+// user can lose the causal link between clicking
+// a link and seeing the browser respond.
+Turbo.config.drive.progressBarDelay = 100

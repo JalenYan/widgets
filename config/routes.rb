07f2af92b16e6ebc965251dc03bcc7711a828fb0
JalenYan <jinlianggg@outlook.com>
2025-07-16 22:43:44 +0800

    原子类、Design System Docs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resources :widgets, only: [ :show, :index ]
   resources :widget_ratings, only: [ :create ]
+  if Rails.env.development?
+    resources :design_system_docs, only: [ :index ]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.

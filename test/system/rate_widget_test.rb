0d8f51054d7d12c0b7c63bb916d321154cd36f2c
JalenYan <jinlianggg@outlook.com>
2025-07-21 14:52:28 +0800

    无头浏览器测试 js，js 有问题，那么测试也应该失败

---
+++ test/system/rate_widget_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+class RateWidgetTest < BrowserSystemTestCase
+  test "rate a widget shows our rating inline" do
+    visit widget_path(1234)
+
+    click_on "2"
+
+    assert_selector "widget-rating [role=status]", text: /thanks for rating.*2/im
+  end
+end

89795f2d8a0d5687eec127cfed5942d249faeca1
JalenYan <jinlianggg@outlook.com>
2025-07-21 14:42:37 +0800

    无头浏览器测试环境配置

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,4 +1,21 @@
 require "test_helper"
+
+Capybara.register_driver :root_headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.chrome(
+    args: [
+      "headless",
+      "disable-gpu",
+      "no-sandbox",
+      "disable-dev-shm-usage",
+      "whitelisted-ips"
+    ],
+    logging_prefs: {
+      browser: "ALL"
+    }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end # register_driver
+
 require "support/with_clues"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
@@ -23,4 +40,9 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     assert_selector "[data-testid='widget-name']", text: widgets_name_regexp
     assert_selector "h2", text: formatted_widget_id_regexp
   end
+end
+
+# Base test class for system tests requiring JavaScript
+class BrowserSystemTestCase < ApplicationSystemTestCase
+  driven_by :root_headless_chrome, screen_size: [ 1400, 1400 ]
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,18 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+Capybara.configure do |config|
+  # This allows helpers like click_on to locate
+  # any object by data-testid in addtion to
+  # built-in selector-like values
+  #
+  # example:
+  # <button data-testid="submit-button">提交</button>
+  # <div data-testid="error-message">错误信息</div>
+  # click_on "submit-button"           # 通过 data-testid 定位
+  # assert_selector "error-message"    # 通过 data-testid 定位
+  config.test_id = "data-testid"
+end
 
 module ActiveSupport
   class TestCase

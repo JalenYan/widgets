e03d0901f260992719d85d95a86378b4aadb009e
JalenYan <jinlianggg@outlook.com>
2025-07-21 12:08:37 +0800

    必要时使用 data-testid 专门用于测试的 dom 查找。
    - hint：当某个测试第一次因为 DOM 的变动而不得不修改时，就停止依赖 DOM 进行断言，转而使用 data-testid

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -20,7 +20,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     # remember, 1234 is formatted as 12.34
     formatted_widget_id_regexp = /12\.34/
 
-    assert_selector "h1", text: widgets_name_regexp
+    assert_selector "[data-testid='widget-name']", text: widgets_name_regexp
     assert_selector "h2", text: formatted_widget_id_regexp
   end
 end

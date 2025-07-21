565184248b607f7e740e5f8f82287cf547045c3c
JalenYan <jinlianggg@outlook.com>
2025-07-21 16:12:31 +0800

    生成数据库迁移

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_21_080106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "manufacturers", force: :cascade do |t|
+    t.text "name"
+    t.text "address"
+    t.text "city"
+    t.text "post_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.text "name"
+    t.text "status"
+    t.bigint "manufacturer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["manufacturer_id"], name: "index_widgets_on_manufacturer_id"
+  end
+
+  add_foreign_key "widgets", "manufacturers"
 end

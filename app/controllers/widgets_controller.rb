c3ee56dddca27e176385867ec6df50d97fa0635a
JalenYan <jinlianggg@outlook.com>
2025-07-25 14:57:09 +0800

    LabelWithErrorComponent 组件，用于显示字段验证错误

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -5,6 +5,9 @@ class WidgetsController < ApplicationController
   def new
     @widget = Widget.new
     @manufacturers = Manufacturer.all
+    @widget.errors.add(:name,           :blank)
+    @widget.errors.add(:manufacturer_id,:blank)
+    @widget.errors.add(:price_cents,    :not_a_number)
   end
 
   def create

1dffcc0df44338aa47fcabc9f5266da115c66b5e
JalenYan <jinlianggg@outlook.com>
2025-07-25 18:10:59 +0800

    针对 widget_creator 进行测试驱动开发

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -3,7 +3,7 @@ class Widget < ApplicationRecord
   belongs_to :widget_status
 
   validates :price_cents, numericality: { less_than_or_equal_to: 10_000_00 }
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 5 }
   validates :manufacturer_id, presence: true
 
   def self.fresh = self.where(status: "fresh")

21f1bdbc99ada0ddf2080bbc51765705c3c82337
JalenYan <jinlianggg@outlook.com>
2025-07-25 16:22:27 +0800

    编写 system test

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,43 +3,28 @@ require "ostruct"
 class WidgetsController < ApplicationController
   def new
     @widget = Widget.new
+
     @manufacturers = Manufacturer.all
-    @widget.errors.add(:name,           :blank)
-    @widget.errors.add(:manufacturer_id,:blank)
-    @widget.errors.add(:price_cents,    :not_a_number)
   end
 
   def create
-    render plain: "Thanks"
+    @widget = Widget.create(
+      name: params.require(:widget)[:name],
+      price_cents: params.require(:widget)[:price_cents],
+      manufacturer_id: params.require(:widget)[:manufacturer_id],
+      widget_status: WidgetStatus.first
+    )
+
+    if @widget.valid?
+      redirect_to widget_path(@widget)
+    else
+      @manufacturers = Manufacturer.all
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-    manufacturer = OpenStruct.new(
-      id: rand(100),
-      name: "Sector 7G",
-      address: OpenStruct.new(
-        id: rand(100),
-        country: "CN"
-      )
-    )
-
-    widget_name = params[:id].to_i == 1234 ? "Stembolt" : "Widget #{params[:id]}"
-
-    @widget = OpenStruct.new(
-      id: params[:id],
-      manufacturer_id: manufacturer.id,
-      name: widget_name,
-      manufacturer: manufacturer
-    )
-
-    # 因为现在还没有 widget 类，所以这样演示
-    def @widget.widget_id
-      if self.id.to_s.length < 3
-        self.id.to_s
-      else
-        self.id.to_s[0..-3] + "." + self.id.to_s[-2..-1]
-      end
-    end
+    @widget = Widget.find(params[:id])
   end
 
   def index
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -3,6 +3,8 @@ class Widget < ApplicationRecord
   belongs_to :widget_status
 
   validates :price_cents, numericality: { less_than_or_equal_to: 10_000_00 }
+  validates :name, presence: true
+  validates :manufacturer_id, presence: true
 
   def self.fresh = self.where(status: "fresh")
 

a32aaeb73dc631c6dc21c07a2c7aca4cd6a7915c
JalenYan <jinlianggg@outlook.com>
2025-07-27 17:45:39 +0800

    测试 controller

---
+++ test/controllers/widgets_controller_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class WidgetsControllerTest < ActionDispatch::IntegrationTest
+  test "converts dollars to cents when creating widget" do
+    manufacturer = FactoryBot.create(:manufacturer)
+    FactoryBot.create(:widget_status, name: "Fresh")
+
+    post widgets_url, params: {
+      widget: {
+        name: "Test Widget",
+        price_cents: "123.45",
+        # 前端传入的所有数据都是字符串，测试时保持一致
+        manufacturer_id: manufacturer.id.to_s
+        # 错误示例
+        # manufacturer: manufacturer
+      }
+    }
+
+    widget = Widget.last
+    refute_nil widget
+    assert_redirected_to widget_url(widget)
+    assert_equal 12345, widget.price_cents
+  end
+end

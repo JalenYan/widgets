786cc70a0161e26d86d5e27fb60e8e7251721751
JalenYan <jinlianggg@outlook.com>
2025-07-28 12:40:51 +0800

    api key 数据库迁移

---
+++ db/migrate/20250728042956_create_api_keys.rb
@@ -0,0 +1,28 @@
+class CreateApiKeys < ActiveRecord::Migration[8.0]
+  def change
+    create_table :api_keys do |t|
+      t.text :key, null: false,
+        comment: "The actual key clients should use"
+
+      t.text :client_name, null: false,
+        comment: "Name of the client who was assigned this key"
+
+      t.datetime :created_at, null: false,
+        comment: "When this key was created"
+
+      t.datetime :deactivated_at, null: true,
+        comment: "When the key was deactivated. " +
+                  "When present, this key is not valid."
+
+      # Note: No updated_at because there should be no updates
+      #       to rows here other than to deactivate
+    end
+    add_index :api_keys, :key, unique: true,
+              comment: "API keys have to be unique or we don't who is accessing us"
+
+    add_index :api_keys, :client_name,
+              unique: true,
+              where: "deactivated_at IS NULL",
+              comment: "We need to be able to find keys by client"
+  end
+end

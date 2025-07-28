561e5e1dd31b6e04eca09cbe5fb1fd3ec757a2aa
JalenYan <jinlianggg@outlook.com>
2025-07-28 12:29:20 +0800

    HTTP Basic authentication

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,6 @@
 class ApiController < ApplicationController
+  skip_before_action :require_login # or whatever callback was
+
+  http_basic_authenticate_with name: ENV["API_USERNAME"],
+                               password: ENV["API_PASSWORD"]
 end
